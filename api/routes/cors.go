9eb56ab61cfaed6aeaff86dc18f51f5cc2a86d55
EricLau1 <ericlau.oliveira@gmail.com>
2019-03-10 15:56:39 -0300

    correções

--- api/routes/cors.go
+++ api/routes/cors.go
@@ -8,6 +8,6 @@ import (
 func LoadCors(r http.Handler) http.Handler {
 	headers := handlers.AllowedHeaders([]string{"X-Request", "Content-Type", "Authorization"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
-	origins := handlers.AllowedMethods([]string{"*"})
+	origins := handlers.AllowedOrigins([]string{"*"})
 	return handlers.CORS(headers, methods, origins)(r)
 }
